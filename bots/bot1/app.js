cf964be7090c1f882ae408c717df3f0d32dcdc06
dmitrypapka1 <dmitry.papka@marfeel.com>
2019-09-26 15:02:14 -0300

    generating apps

--- bots/bot1/app.js
+++
@@ -1,5 +0,0 @@
-const request = require('request');
-const lodash = require('lodash');
-
-console.log(typeof request);
-console.log(typeof lodash);
--- bots/bot2/app.js
+++
@@ -1,5 +0,0 @@
-const request = require('request');
-const express = require('express');
-
-console.log(typeof request);
-console.log(typeof express);
